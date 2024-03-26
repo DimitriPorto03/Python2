de7d3e5f6c70968d0e2b6fc87bfa1f43ac82c402
DimitriPorto03 <133895461+DimitriPorto03@users.noreply.github.com>
2024-03-26 14:51:00 -0300

    Update ex04.py

--- exercicio/primeiro_trimestre/lista_prova/ex04.py
+++ exercicio/primeiro_trimestre/lista_prova/ex04.py
@@ -1,1 +1,7 @@
+nota1 = int(input("Digite a nota do 1º bimestre: "))
+nota2 = int(input("Digite a nota do 2º bimestre: "))
+nota3 = int(input("Digite a nota do 3º bimestre: "))
+nota4 = int(input("Digite a nota do 4º bimestre: "))
 
+media = (nota1+nota2+nota3+nota4)//4
+print("Sua média foi ",media)
