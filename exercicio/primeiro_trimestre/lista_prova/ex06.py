8ae5a497030fbf06971a8d85d88cf750bca79c1d
DimitriPorto03 <133895461+DimitriPorto03@users.noreply.github.com>
2024-03-27 11:50:45 -0300

    Update ex06.py

--- exercicio/primeiro_trimestre/lista_prova/ex06.py
+++ exercicio/primeiro_trimestre/lista_prova/ex06.py
@@ -1,1 +1,9 @@
+def sum(a, b, c):
+    return (a + b + c)
+
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o segundo número: "))
+c = int(input("Digite o terceiro número: "))
+
+print(f"A soma de {a}, {b} e {c} é igual a {sum(a, b, c)}")
 
