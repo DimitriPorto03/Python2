a003dab81912214ec4a2c0ed9d2c02c4e2d904b2
DimitriPorto03 <133895461+DimitriPorto03@users.noreply.github.com>
2024-05-06 18:06:01 -0300

    Update consulta.py

--- trabalhos/calculo_enem/consulta.py
+++ trabalhos/calculo_enem/consulta.py
@@ -1,11 +1,11 @@
 import sqlite3
 
 # Conectar ao banco de dados
-conn = sqlite3.connect('dados_usuarios.db')
+conn = sqlite3.connect('dados_alunos.db')
 cursor = conn.cursor()
 
 # Executar uma consulta SQL para selecionar todos os registros da tabela usuarios
-cursor.execute("SELECT * FROM usuarios")
+cursor.execute("SELECT * FROM alunos")
 
 # Recuperar todos os resultados da consulta
 resultados = cursor.fetchall()
