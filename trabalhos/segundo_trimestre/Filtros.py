4446c0311101e96af100a7427a489d0f82b8a293
DimitriPorto03 <133895461+DimitriPorto03@users.noreply.github.com>
2024-05-21 12:26:54 -0300

    Update Filtros.py

--- trabalhos/segundo_trimestre/Filtros.py
+++ trabalhos/segundo_trimestre/Filtros.py
@@ -1,1 +1,17 @@
+from PIL import Image
 
+#Abrindo imagem
+imagem = Image.open('r34.jpg')
+
+#Obtendo as dimensões da imagem
+largura, altura = imagem.size
+print(largura)
+print(altura)
+
+for x in range(altura):
+  for y in range(altura):
+    pixel=imagem.getpixel((x,y))
+    novo_pixel=tuple(255-valor for valor in pixel)
+    imagem.putpixel((x,y),novo_pixel)
+
+imagem.save('r34_negativo.jpg')
